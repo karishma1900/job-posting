df8c4d1230b250bcab7c08e777a0e97ac68ae07e
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-10-20 11:56:18 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
